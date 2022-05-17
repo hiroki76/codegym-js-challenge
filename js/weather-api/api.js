78e5128e48ff35039995e67a4dfb24c5ffef4687
hiroki76 <hn072401@gmail.com>
2022-05-17 12:33:32 +0000

    [add]APIエラー時処理実装および変数とオブジェクト名の修正

--- js/weather-api/api.js
+++ js/weather-api/api.js
@@ -1,12 +1,21 @@
-const setWaeather = (location) =>{
+const setWaeather = (location) => {
+    const url = new URL('https://api.openweathermap.org/data/2.5/weather?')
     const queryParams = new URLSearchParams({
         appid: "4b5774e9f3d2a07b84f0f2f88e486224", 
         q: location,
         lang:"ja" 
-    });
-    const myRequest = new Request("https://api.openweathermap.org/data/2.5/weather?" + queryParams)
-    fetch(myRequest)
-    .then(response => response.json())
+    })
+    const request = new Request(url + queryParams)
+    fetch(request)
+    .then((response) => {
+        if (!response.ok) {
+            console.error('サーバーエラー')
+        }
+        return response.json()
+    })
+    .catch(error => {
+        console.error('通信に失敗しました', error)
+    })
     .then(data => {
         const elementWeatherDescription = document.getElementById("weatherDescription")
         elementWeatherDescription.innerHTML = data.weather[0].description
@@ -21,6 +30,6 @@ setWaeather("London")
 const selector = document.querySelector('#LocationSelect')
 selector.addEventListener('change', (event) => {
     const box = document.querySelector('#location')
-    const box2 = event.target.value
-	setWaeather(box2)
+    const location = event.target.value
+    setWaeather(location)
 })
